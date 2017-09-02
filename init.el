fb80fde4737ec362d3c48f376fe9f3539b7cb326
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2017-09-02 01:14:56 +0100

    some changes in org config

--- init.el
+++ init.el
@@ -1,1 +1,4 @@
+(add-to-list 'load-path "~/.emacs.d/lisp/org-mode/lisp")
+(add-to-list 'load-path "~/.emacs.d/lisp/org-mode/contrib/lisp")
+(require 'org)
 (org-babel-load-file "~/.emacs.d/configuration.org")
