04988c9d1236fc7246f8472e7eb46bbdf6966a57
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2018-11-11 10:15:03 +0000

    Changed a lot of code

--- hosts/init-alex-pc.el
+++ hosts/init-alex-pc.el
@@ -2,8 +2,8 @@
   '(
     "Iosevka Term-10.5"
     ;; "Iosevka Term-12"
-    ;; "Hack-10.5"
-    "Ubuntu Mono-12"
+    "Hack-10.5"
+    ;; "Ubuntu Mono-12"
     ;; "Menlo for Powerline-10.5"
     ;; "Menlo for Powerline-9"
     ;; "Ubuntu Mono-10.5"
@@ -13,19 +13,35 @@
 (setq my/big-font "Hack-22")
 (setq my/current-font 1)
 
-(setq my/avail-themes
-  '(
-    ;; my/abyss
-    ;; my/nimbus
-    ;; my/blackboard
-    my/gotham
-    (zenburn dark t)
-    ;; ron-swanson
-    (leslie-knope light)
-    ))
-(setq my/current-theme 0)
+(setq
+ sunburn-override-colors-alist
+ '(("sunburn-green" . "#91b791") ("sunburn-bg" . "#433d44")))
 
-(setq my/hl-line-contrast 8)
+(setq my/avail-themes
+  `(
+    ;; ,(assoc 'gotham my/all-themes)
+    ;; ,(assoc 'zenburn my/all-themes)
+    ,(assoc 'blackboard my/all-themes)
+    ;; ,(assoc 'leslie-knope my/all-themes)
+    (sunburn
+     (progn
+       (custom-theme-set-faces
+        'sunburn
+        '(mode-line-buffer-id ((t (:weight bold))))
+        '(minibuffer-prompt ((t (:foreground "#f0dfaf"))))
+        '(fringe ((t (:background "#4f4951"))))
+        '(region ((t (:background "#6b5f6f"))))
+        '(mode-line
+          ((t (:box
+               (:line-width -1 :color nil :style released-button)
+               :foreground "#f1f1f1" :background "#3d344c"))))
+        )
+       (setq pdf-view-midnight-colors '("#dedded" . "#413b42"))
+       (my/hl-line-set-bg) (my/org-set-src-faces)))
+    ))
+(setq my/current-theme 1)
+
+(setq my/hl-line-contrast 5)
 
 (add-hook 'prog-mode-hook
           (lambda ()
