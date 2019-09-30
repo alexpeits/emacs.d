230bd7edea6e568a98bc57e3429bdcbd5de2cb29
Alex Peitsinis <alex.peitsinis@habito.com>
2019-09-30 10:00:21 +0100

    update

---
+++ hosts/init-mac.el
@@ -0,0 +1,12 @@
+;; fonts
+
+(setq my/avail-fonts
+      '(
+        "Menlo-12"
+        "Menlo-14"
+        "Menlo-22"
+        ))
+
+(setq mac-command-modifier 'meta)
+(add-hook 'prog-mode-hook 'hl-line-mode)
+
