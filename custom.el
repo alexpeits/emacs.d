2edebfddfa58825576d45e96f2b14ffde1d7ff83
Alex Peitsinis <alex.peitsinis@habito.com>
2019-09-30 11:06:57 +0100

    update gitignore

--- custom.el
+++ custom.el
@@ -18,5 +18,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-bullets ox-twbs symbol-overlay zoom zeal-at-point yasnippet-snippets yaml-mode xah-math-input which-key web-mode use-package treemacs-projectile terraform-mode smex rjsx-mode restclient racer purescript-mode psc-ide proof-general prettier-js persp-projectile pdf-tools nix-mode nginx-mode magit-todos ledger-mode json-mode intero imenu-list idris-mode highlight-numbers highlight-indent-guides git-timemachine flycheck-yamllint flycheck-rust flx fill-column-indicator expand-region evil-visualstar evil-surround evil-smartparens evil-nerd-commenter evil-leader evil-anzu engine-mode emmet-mode elfeed-web elfeed-goodies dockerfile-mode direnv dired-subtree dired-filter diminish diff-hl dhall-mode deft dash-at-point counsel-projectile company-quickhelp company-anaconda cargo buffer-move bazel-mode ag)))
+ )
