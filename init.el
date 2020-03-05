252600c3c70bd38bdadd29b16ad0fe8f3a43be7b
Alex Peitsinis <alexpeitsinis@gmail.com>
2020-03-05 13:01:06 +0000

    even more use-package optimizations

--- init.el
+++ init.el
@@ -1,5 +1,15 @@
-(setq gc-cons-threshold 100000000)
-(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
+;; https://github.com/hlissner/doom-emacs/blob/develop/docs/faq.org#how-does-doom-start-up-so-quickly
+(setq gc-cons-threshold 100000000
+      gc-cons-percentage 0.6
+      tmp--file-name-handler-alist file-name-handler-alist
+      file-name-handler-alist nil)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold 800000
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist tmp--file-name-handler-alist)
+            (makunbound 'tmp--file-name-handler-alist)
+            ))
 
 (require 'org)
 (org-babel-load-file (concat user-emacs-directory "configuration.org"))
