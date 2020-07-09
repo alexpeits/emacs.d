632e8d7b00aa6e77dda9845bd7f41f42ec909873
Alex Peitsinis <alexpeitsinis@gmail.com>
2020-07-09 17:06:14 +0100

    stuff

--- custom-themes/functor-theme.el
+++ custom-themes/functor-theme.el
@@ -194,7 +194,7 @@
    `(powerline-inactive1             ((t (:inherit powerline-inactive0))))
    `(powerline-inactive2             ((t (:inherit powerline-inactive0))))
 
-   `(minibuffer-prompt ((t (:foreground ,cyan-1 :weight bold))))
+   `(minibuffer-prompt ((t (:foreground ,blue-bright))))
 
    `(persp-selected-face ((t (:foreground ,green-alt :weight bold))))
 
